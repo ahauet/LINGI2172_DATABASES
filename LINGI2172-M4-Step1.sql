246dcc46e7622b9f1d1219347c525ee6a978516e
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2016-04-28 16:16:13 +0200

    acquire table

--- LINGI2172-M4-Step1.sql
+++ LINGI2172-M4-Step1.sql
@@ -7,33 +7,46 @@ DROP TABLE IF EXISTS payment;
 DROP TABLE IF EXISTS ordered_drink;
 DROP TABLE IF EXISTS "order";
 DROP TABLE IF EXISTS drink;
+DROP TABLE IF EXISTS placement;
 DROP TABLE IF EXISTS client;
 DROP TABLE IF EXISTS "table";
 
+DROP SEQUENCE IF EXISTS table_id_seq;
+DROP SEQUENCE IF EXISTS token_id_seq;
+DROP SEQUENCE IF EXISTS drink_id_seq;
+DROP SEQUENCE IF EXISTS order_id_seq;
+DROP SEQUENCE IF EXISTS payment_id_seq;
 
 
+CREATE SEQUENCE table_id_seq;
 CREATE TABLE "table" (
-    table_id integer NOT NULL PRIMARY KEY,
-    codebar varchar(12) NOT NULL
+    table_id integer NOT NULL PRIMARY KEY DEFAULT nextval('table_id_seq')
+    /*codebar varchar(12) NOT NULL*/
 );
 
-
+CREATE SEQUENCE token_id_seq;
 CREATE TABLE client (
-    token_id integer NOT NULL PRIMARY KEY,
-    placed_at integer NOT NULL UNIQUE REFERENCES "table"(table_id)
+    token_id integer NOT NULL PRIMARY KEY DEFAULT nextval('token_id_seq')
+    /*placed_at integer NOT NULL UNIQUE REFERENCES "table"(table_id)*/
 );
 
+CREATE TABLE placement (
+    client_id integer NOT NULL REFERENCES client(token_id),
+    table_id integer NOT NULL REFERENCES "table"(table_id),
+    PRIMARY KEY (client_id, table_id)
+) ;
 
+CREATE SEQUENCE drink_id_seq;
 CREATE TABLE drink (
-    drink_id integer NOT NULL PRIMARY KEY,
+    drink_id integer NOT NULL PRIMARY KEY DEFAULT nextval('drink_id_seq'),
     price numeric(18,2) NOT NULL,
     name varchar(30) NOT NULL,
     description varchar(500)
 );
 
-
+CREATE SEQUENCE order_id_seq;
 CREATE TABLE "order" (
-    order_id integer NOT NULL PRIMARY KEY,
+    order_id integer NOT NULL PRIMARY KEY DEFAULT nextval('order_id_seq'),
     order_time date NOT NULL,
     passes_by integer NOT NULL REFERENCES client(token_id)
 );
@@ -46,10 +59,9 @@ CREATE TABLE ordered_drink (
     PRIMARY KEY (order_id, drink_id)
 ) ;
 
-
-
+CREATE SEQUENCE payment_id_seq;
 CREATE TABLE payment (
-    payment_id integer NOT NULL PRIMARY KEY,
+    payment_id integer NOT NULL PRIMARY KEY DEFAULT nextval('payment_id_seq'),
     amount_paid numeric(18,2) NOT NULL,
     made_by integer NOT NULL REFERENCES client(token_id)
 );
---
+++ acquire_table.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE FUNCTION acquire_table(codebar integer) RETURNS integer AS $$
+    DECLARE
+      next_id integer := nextval('token_id_seq');
+    BEGIN
+      INSERT INTO client (token_id) values (next_id);
+      INSERT INTO placement (client_id, table_id) values (next_id, codebar);
+      return next_id;
+    END;
+    $$ LANGUAGE plpgsql;
